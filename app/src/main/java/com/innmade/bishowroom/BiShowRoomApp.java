032540b64dc9d5d4ca7564f2c254b3f8c9c47a8e
Varol <>
2022-03-19 14:07:51 +0300

    add network listener

---
+++ app/src/main/java/com/innmade/bishowroom/BiShowRoomApp.java
@@ -0,0 +1,68 @@
+package com.innmade.bishowroom;
+
+import android.app.Application;
+import android.content.res.Configuration;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkCapabilities;
+import android.net.NetworkRequest;
+
+import androidx.annotation.NonNull;
+
+public class BiShowRoomApp extends Application {
+
+    private static boolean activityVisible;
+
+    //Network Manager
+    static ConnectivityManager.NetworkCallback networkCallback;
+    static ConnectivityManager connectivityManager;
+    static NetworkRequest networkRequest;
+
+    public void setNetworkCallback(ConnectivityManager.NetworkCallback networkCallback) {
+        BiShowRoomApp.networkCallback = networkCallback;
+        connectivityManager.requestNetwork(networkRequest, BiShowRoomApp.networkCallback);
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        networkRequest = new NetworkRequest.Builder()
+                .addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+                .addTransportType(NetworkCapabilities.TRANSPORT_WIFI)
+                .addTransportType(NetworkCapabilities.TRANSPORT_CELLULAR)
+                .build();
+        networkCallback = new ConnectivityManager.NetworkCallback() {
+            @Override
+            public void onAvailable(@NonNull Network network) {
+                super.onAvailable(network);
+            }
+
+            @Override
+            public void onLost(@NonNull Network network) {
+                super.onLost(network);
+            }
+
+
+        };
+        connectivityManager = getSystemService(ConnectivityManager.class);
+        connectivityManager.requestNetwork(networkRequest, networkCallback,5000);
+
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+    }
+    public static boolean isActivityVisible() {
+        return activityVisible;
+    }
+
+    public static void activityResumed() {
+        activityVisible = true;
+    }
+
+    public static void activityPaused() {
+        activityVisible = false;
+    }
+}
